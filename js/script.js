bce76288555a06a644f3169fbeb95b1f852f7c3f
shahboz0909 <shahbozahmadov8@gmail.com>
2022-04-08 22:43:47 +0500

    git init .

---
+++ js/script.js
@@ -0,0 +1,15 @@
+alert("xush kelibsiz");
+
+var firstName = prompt("ismingiz nma?");
+
+var lastName = prompt("Familiyangiz nma?");
+
+var age = prompt("Yoshingiz nechida?");
+
+var work = prompt("Qayerda ishlaysiz?");
+
+var position = prompt("Lavozimingiz qanaqa?");
+
+alert("tabriklaymiz ro'yxatdan o'tdingiz");
+
+console.log(`ismi - ${firstName} \nFamiliyasi - ${lastName} \nYoshi - ${age} \nishlash joyi - ${work} \nLavozimi - ${position}`);
